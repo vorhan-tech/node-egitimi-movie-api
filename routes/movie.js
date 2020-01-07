4b3ffbe4cf5ad31849003089a00852d87e5d3a29
unknown <volkanorhan33@gmail.com>
2020-01-07 09:22:21 +0300

    /api/directors POST method

--- routes/movie.js
+++ routes/movie.js
@@ -12,6 +12,19 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({})
+    .limit(10)
+    .sort({ imdb_score: -1 });
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 router.get('/:movie_id', (req, res, next) => {
   const promise = Movie.findById(req.params.movie_id);
   promise
@@ -26,8 +39,24 @@ router.get('/:movie_id', (req, res, next) => {
     });
 });
 
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndRemove(req.params.movie_id);
+  promise
+    .then(movie => {
+      if (!movie) {
+        next({ message: 'The movie was not found', code: 99 });
+      }
+      res.json({ status: 1 });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 router.put('/:movie_id', (req, res, next) => {
-  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body);
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {
+    new: true
+  });
   promise
     .then(movie => {
       if (!movie) {
@@ -46,7 +75,34 @@ router.post('/', (req, res, next) => {
   const promise = movie.save();
   promise
     .then(data => {
-      res.json({ status: 1 });
+      res.json({ data });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({})
+    .limit(10)
+    .sort({ imdb_score: -1 });
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get('/between/:start_year/:end_year', (req, res) => {
+  const { start_year, end_year } = req.params;
+  const promise = Movie.find({
+    year: { $gte: parseInt(start_year), $lte: parseInt(end_year) }
+  });
+  promise
+    .then(data => {
+      res.json(data);
     })
     .catch(err => {
       res.json(err);
